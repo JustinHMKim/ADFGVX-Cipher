8cd0a3f6ff47fde68f1d01f1bbce97c3096c795f
JustinHMKim <35758501+JustinHMKim@users.noreply.github.com>
2019-02-01 22:03:06 -0800

    Replaced some hardcoded inputs, added cipher definition

--- main.cpp
+++ main.cpp
@@ -7,37 +7,51 @@ using std::cout; using std::string; using std::vector; using std::cout; using st
 
 void adfgvxEncrypt(string plaintext, string keyword, string sorted_keyword, vector<char> &ciphertxt);
 int chartoint(char let);
+int invalidin();
 
 int main()
 {
-
     char cont = 'n';
     vector<char>final_ct;
+
+    int choice;
     do{
-        string pt;// = "PRODUCTCIPHERS";
-        cout<<"Enter the plaintext you'd like to encrypt: ";
-        cin>>pt;
-        cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-        std::transform(pt.begin(),pt.end(),pt.begin(), :: toupper);
-        string kw;//= "DEUTSCH";
-
-        cout<<"Enter the keyword you'd like to use: ";
-        cin>>kw;
-        std::transform(kw.begin(),kw.end(),kw.begin(), :: toupper);
-        string skw = kw;
-        sort(skw.begin(),skw.end());
-        //cout<<endl<<keyword<<" "<<skw<<endl;
-
-        adfgvxEncrypt(pt,kw,skw, final_ct);
-        cout<<"Your encrypted message: ";
-        cout<<endl;
-        for (unsigned index = 0; index < final_ct.size(); ++index){
-            cout<<final_ct[index];
+        cout<<"Would you like to 1. encrypt a message or 2. learn about the ADFGVX cipher? ";
+        cin>>choice;
+        if (choice != 1 && choice != 2){
+            choice = invalidin();
         }
-        cout<<endl;
-        final_ct.clear();
 
-        cout<<"Would you like to continue? (Y/N)";
+        if (choice == 1){
+            string kw;//= "DEUTSCH";
+            cout<<"Enter the keyword you'd like to use: ";
+            cin>>kw;
+            cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+            std::transform(kw.begin(),kw.end(),kw.begin(), :: toupper);
+            string skw = kw;
+            sort(skw.begin(),skw.end());
+            //cout<<endl<<keyword<<" "<<skw<<endl;
+            string pt;// = "PRODUCTCIPHERS";
+            cout<<"Enter the plaintext you'd like to encrypt: ";
+            cin>>pt;
+            std::transform(pt.begin(),pt.end(),pt.begin(), :: toupper);
+            final_ct.clear();
+            cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+            adfgvxEncrypt(pt,kw,skw, final_ct);
+            cout<<"Your encrypted message is: ";
+            cout<<endl;
+            for (unsigned index = 0; index < final_ct.size(); ++index){
+                cout<<final_ct[index];
+            }
+            cout<<endl;
+        }
+        else if (choice == 2){
+            cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+            cout<<"The Germany Army's ADFGVX cipher used during World War I. One of the earliest product ciphers. The key has two parts: the first part is a permutation of the 26 letters and 10 digits. The second is a keyword with no repeated letters."<<endl<<"Press any key to continue";
+            cin.get();
+            cout<<"In World War II it was shown that alternating substitution and transposition ciphers is a very secure thing to do. ADFGVX is weak since the substitution and transposition each occur once and the substitution is fixed, not key controlled."<<endl<<"-Dr. Edward Schaefer"<<endl;
+        }
+        cout<<endl<<"Would you like to continue? (Y/N): ";
         cin>>cont;
         cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
     }while(cont == 'y' || cont == 'Y');
@@ -52,14 +66,13 @@ void adfgvxEncrypt(string plaintext, string keyword, string sorted_keyword, vect
     char key[6][6];
 
     string key_components = "KZWR1F9B6CL5Q7JPGXEVY3AN8ODH02U4ISTM";
-    std::random_shuffle(key_components.begin(), key_components.end());
+    std::random_shuffle(key_components.begin(), key_components.end());//gives a random keymap for each encryption
     for (int r = 0; r < 6 ; ++r){
         for (int q =0; q < 6; ++q){
             key[r][q] = key_components[r*6+q];
         }
     }
-
-    //reference keymap, used for initial test of accuracy
+    //reference keymap, used for initial accuracy test
     /*
     char key[6][6] = {
     {'K', 'Z', 'W', 'R', '1', 'F'},
@@ -70,11 +83,9 @@ void adfgvxEncrypt(string plaintext, string keyword, string sorted_keyword, vect
     {'U', '4', 'I', 'S', 'T', 'M'}
     };
     */
-
     cout<<"Your key is: "<<endl<<endl;
     char ADFGVX[6] = {'A','D','F','G','V','X'};
     int adfgvx_vert = 0;
-
     //displays the key, ADFVGX and lines for readability
     for (int key_horiz = 0; key_horiz <6 ; ++key_horiz){
         if (key_horiz == 0){
@@ -91,8 +102,8 @@ void adfgvxEncrypt(string plaintext, string keyword, string sorted_keyword, vect
         }
         cout<<endl;
     }
-    cout<<endl;
-
+    cout<<endl<<"Press any key to continue"<<endl;
+    cin.get();
     //finds each character of the plaintext in the key 2d array
     for (unsigned ptind = 0; ptind < vecplt.size(); ++ptind){
         for (int i  = 0; i < 6; ++i){
@@ -106,19 +117,20 @@ void adfgvxEncrypt(string plaintext, string keyword, string sorted_keyword, vect
             }
         }
     }
+    cout<<endl<<"Press any key to continue"<<endl;
+    cin.get();
     cout<<endl<<plaintext<<" outputs ";
     for (unsigned k = 0; k < tenta_ct.size(); ++k){
         cout<<tenta_ct[k];
     }
-
-    cout<<endl<<endl;
-
+    cout<<endl<<endl<<"Press any key to continue"<<endl;
+    cin.get();
     vector<int>order;
     vector<int>kw_ord;
     for(unsigned kwlen = 0; kwlen < keyword.length();++kwlen){
         kw_ord.push_back(chartoint(keyword[kwlen]));
     }
-    for (unsigned kwind = 0; kwind < kw_ord.size(); ++kwind){
+    for (unsigned kwind = 0; kwind < kw_ord.size(); ++kwind){//for the sake of visualisation, shows order of the columns in the final ciphertext
         int letter_val = kw_ord[kwind];
         int letter_pos = 1;
         for (unsigned kwind2 = 0; kwind2 < kw_ord.size(); ++kwind2){
@@ -126,7 +138,6 @@ void adfgvxEncrypt(string plaintext, string keyword, string sorted_keyword, vect
             if(letter_val > next){
                 ++letter_pos;
             }
-
         }
         order.push_back(letter_pos);
     }
@@ -139,7 +150,6 @@ void adfgvxEncrypt(string plaintext, string keyword, string sorted_keyword, vect
     else{
         ct_mat_rows = 1+((tenta_ct.size())/keyword.length());
     }
-
     for(unsigned kw_len = 0; kw_len < keyword.length();++kw_len){
         cout<<keyword[kw_len]<<"  ";
     }
@@ -158,33 +168,31 @@ void adfgvxEncrypt(string plaintext, string keyword, string sorted_keyword, vect
     {
         for(unsigned j=0;j<keyword.length();++j)
         {
-
-            if(tenta_ct.size()<=(7*i+j)){
+            if(tenta_ct.size()<=(keyword.length()*i+j)){//in case the plaintext message doesn't divide evenly, still ensures the full message is encrypted
                 char space = ' ';
-                (ct_mat[i])[j] =(space);
+                (ct_mat[i])[j] =(space);//concatenation prevents the message from being decrypted properly
             }
             else{
-                (ct_mat[i])[j] = tenta_ct[7*i+j];
+                (ct_mat[i])[j] = tenta_ct[keyword.length()*i+j];//enters into the 6x6 array to be scrambled
             }
             cout<<ct_mat[i][j]<<"  ";
         }
         cout<<endl;
     }
-
+    cout<<endl<<"Press any key to continue"<<endl;
+    cin.get();
     for (unsigned keyword_count = 0; keyword_count < keyword.length(); ++keyword_count){
         for (unsigned position = 0; position < keyword.length(); ++position){
             for(int sorted_row=0;sorted_row<ct_mat_rows;++sorted_row){
                 if (sorted_keyword[keyword_count] == keyword[position]){
-                    if(ct_mat[sorted_row][position] != ' '){
+                    if(ct_mat[sorted_row][position] != ' '){//gets rid of empty spaces, makes ciphertext harder to decrypt if each column isn't equal in length
                         ciphertxt.push_back(ct_mat[sorted_row][position]);
                     }
                 }
             }
         }
     }
-
     cout<<endl;
-
 }
 
 int chartoint(char let){
@@ -274,4 +282,12 @@ int chartoint(char let){
                    break;
            }
     return num_rep;
+}
+
+int invalidin(){
+    cin.clear();
+    int newinput;
+    cout<<"Please enter a valid input: ";
+    cin>>newinput;
+    return newinput;
 }
